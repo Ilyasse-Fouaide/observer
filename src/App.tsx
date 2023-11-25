9a5820f32f20f8606a7776e68f71634b8fa8a981
ilyasse fouaide <ilyasse.fouaide@gmail.com>
2023-11-25 13:55:51 +0100

    Update new version of code to use IntersectionObserver to load more anime on scroll.

--- src/App.tsx
+++ src/App.tsx
@@ -6,50 +6,64 @@ function App() {
   const [page, setPage] = React.useState(1);
 
   const { loading, error, anime, hasMore } = useAnimeFetch(filter, page);
-  console.log(hasMore)
 
   const observer = React.useRef<any>();
 
+  // const lastElement = React.useCallback((node: any) => {
+  //   if (observer.current) observer.current.disconnect();
+  //   observer.current = new IntersectionObserver((entries) => {
+  //     const isIntersecting = entries[0].isIntersecting;
+  //     if (isIntersecting && hasMore) {
+  //       setPage(prev => prev + 1);
+  //     }
+  //     console.log(entries);
+  //   }, { threshold: 1 });
+  //   if (node) observer.current.observe(node)
+  // }, [hasMore, loading]);
+
   const lastElement = React.useCallback((node: any) => {
-    if (observer.current) observer.current.disconnect();
-    if (loading) return
-    observer.current = new IntersectionObserver((entries) => {
-      const isIntersecting = entries[0].isIntersecting;
+    observer.current = new IntersectionObserver((entries, observe) => {
+      const isIntersecting = entries[0].isIntersecting
       if (isIntersecting && hasMore) {
         setPage(prev => prev + 1);
       }
+      if (isIntersecting) {
+        observe.disconnect();
+      }
     });
-    if (node) observer.current.observe(node)
-  }, [hasMore, loading]);
+    if (node) {
+      observer.current.observe(node);
+    }
+  }, [])
 
   return (
     <div className="max-w-5xl mx-auto">
       <div className="mt-20 grid grid-cols-4 gap-6">
         {anime.map((item, key) => {
-          // return (
-          //   <div key={key} ref={lastElement}>
-          //     <div className="w-full aspect-[2/3]">
-          //       <img src={item.images.jpg.large_image_url} alt={item.title} className="w-full h-full object-cover" />
+          return (
+            <div key={key} ref={anime.length - 1 === key ? lastElement : null}>
+              <div className="w-full aspect-[2/3]">
+                <img src={item.images.jpg.large_image_url} alt={item.title} className="w-full h-full object-cover" />
+              </div>
+            </div>
+          )
+          // if (anime.length === key + 1) {
+          //   return (
+          //     <div key={key} ref={lastElement}>
+          //       <div className="w-full aspect-[2/3]">
+          //         <img src={item.images.jpg.large_image_url} alt={item.title} className="w-full h-full object-cover" />
+          //       </div>
           //     </div>
-          //   </div>
-          // )
-          if (anime.length === key + 1) {
-            return (
-              <div key={key} ref={lastElement}>
-                <div className="w-full aspect-[2/3]">
-                  <img src={item.images.jpg.large_image_url} alt={item.title} className="w-full h-full object-cover" />
-                </div>
-              </div>
-            )
-          } else {
-            return (
-              <div key={key}>
-                <div className="w-full aspect-[2/3]">
-                  <img src={item.images.jpg.large_image_url} alt={item.title} className="w-full h-full object-cover" />
-                </div>
-              </div>
-            )
-          }
+          //   )
+          // } else {
+          //   return (
+          //     <div key={key}>
+          //       <div className="w-full aspect-[2/3]">
+          //         <img src={item.images.jpg.large_image_url} alt={item.title} className="w-full h-full object-cover" />
+          //       </div>
+          //     </div>
+          //   )
+          // }
         }
         )}
       </div>
