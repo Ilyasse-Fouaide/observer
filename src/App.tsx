85bc5632f8f019bc62a87ee89df2e7cd8817d7c5
ilyasse fouaide <ilyasse.fouaide@gmail.com>
2023-11-25 12:09:08 +0100

    Update code to display images from the API response

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,22 @@ function App() {
   const [filter, setFilter] = React.useState("bypopularity");
   const [page, setPage] = React.useState(1);
 
-  useAnimeFetch(filter, page);
+  const { loading, error, anime, hasMore } = useAnimeFetch(filter, page);
+
+  console.log(anime)
 
   return (
-    <>
-      <div>APP</div>
-    </>
+    <div className="max-w-5xl mx-auto">
+      <div className="mt-20 grid grid-cols-4 gap-6">
+        {anime.map((item, key) =>
+          <div key={key}>
+            <div className="w-full aspect-[2/3]">
+              <img src={item.images.jpg.large_image_url} alt={item.title} className="w-full h-full object-cover" />
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
   )
 }
 
