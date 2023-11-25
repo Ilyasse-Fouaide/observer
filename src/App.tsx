434bafda75cd3d0843ac91d9a9a1bab1b35f1db8
ilyasse fouaide <ilyasse.fouaide@gmail.com>
2023-11-25 12:35:33 +0100

    Update code to only load more data when the user is viewing the last item

--- src/App.tsx
+++ src/App.tsx
@@ -13,8 +13,9 @@ function App() {
     if (observer.current) observer.current.disconnect();
     observer.current = new IntersectionObserver((entries) => {
       const isIntersecting = entries[0].isIntersecting;
-      if (isIntersecting) {
+      if (isIntersecting && hasMore) {
         console.log("visible");
+        setPage(prev => prev + 1);
       }
     });
     if (node) observer.current.observe(node)
@@ -24,6 +25,13 @@ function App() {
     <div className="max-w-5xl mx-auto">
       <div className="mt-20 grid grid-cols-4 gap-6">
         {anime.map((item, key) => {
+          // return (
+          //   <div key={key} ref={lastElement}>
+          //     <div className="w-full aspect-[2/3]">
+          //       <img src={item.images.jpg.large_image_url} alt={item.title} className="w-full h-full object-cover" />
+          //     </div>
+          //   </div>
+          // )
           if (anime.length === key + 1) {
             return (
               <div key={key} ref={lastElement}>
